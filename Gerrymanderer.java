19a39d3d9d8da726d6677d939aa628f71943c027
Whu-UW <75003398+Whu-UW@users.noreply.github.com>
2020-11-25 15:55:52 -0800

    Update Gerrymanderer.java
    remove test println

--- Gerrymanderer.java
+++ Gerrymanderer.java
@@ -3,6 +3,5 @@ package gairrymander;
 public class Gerrymanderer {
     public static void main(String[] args) {      
         System.out.println("win");
-        System.out.println("test");
     }
 }
