bb295a407ce5dc7c6c10c747ed88f0d9c39dad8c
Whu-UW <75003398+Whu-UW@users.noreply.github.com>
2020-11-25 15:53:50 -0800

    Update Gerrymanderer.java
    first commit test

--- Gerrymanderer.java
+++ Gerrymanderer.java
@@ -1,7 +1,8 @@
 package gairrymander;
 
 public class Gerrymanderer {
-    public static void main(String[] args) {
+    public static void main(String[] args) {      
         System.out.println("win");
+        System.out.println("test");
     }
 }
